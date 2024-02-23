1ecac6b67f14057e692cf3b21642c2e6d5e18397
waseembaloch21 <154684340+waseembaloch21@users.noreply.github.com>
2024-02-23 09:01:01 +0500

    Add files via upload

---
+++ assigment2/app.js
@@ -0,0 +1,20 @@
+// chapter3
+
+// answer1
+
+var num = "I am a 19 year old"
+alert(num);
+
+// answer2
+
+var was = "You have visited this site 14 time"
+alert(was);
+
+document.body.innerHTML='';
+
+
+
+
+
+
+
